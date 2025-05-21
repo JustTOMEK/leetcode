dbb5a9af9889d3d437c001181f7c92e8c866fa2f
tomek_jablko <trunio31@gmail.com>
2025-05-21 16:40:42 +0900

    O(n^2) solution

--- length_of_last_word.py
+++ length_of_last_word.py
@@ -1,3 +1,14 @@
 class Solution:
-    def lengthOfLastWord(self, s: str) -> int:
-        return len(s.split()[-1])
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        maks = ""
+        for i in range(1, len(strs[0]) + 1):
+            check = strs[0][:i]
+            for j in range(len(strs)):
+                if strs[j].startswith(strs[0][:i]):
+                    maks = strs[0][:i]
+                else:
+                    maks = strs[0][:i - 1]
+                    break
+            if len(maks) != i:
+                break
+        return maks
