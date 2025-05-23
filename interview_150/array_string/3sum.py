03b207199bbaa74efcb1aa346095f9f2762f4272
tomek_jablko <trunio31@gmail.com>
2025-05-23 16:08:29 +0900

    O(n^2) solution

--- interview_150/array_string/3sum.py
+++ interview_150/array_string/3sum.py
@@ -1,12 +1,23 @@
-import itertools
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        len_nums = len(nums)
+        answers = []
+        nums.sort()
 
-nums = [-1, 0, 1, 2, -1, -4]
-answers = []
-for combo in itertools.combinations(nums, 3):
-    if sum(combo) == 0:
-        lst = list(combo)
-        lst.sort()
-        if lst not in answers:
-            answers.append(lst)
+        for i in range(len_nums - 2):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            j = i + 1
+            k = len_nums - 1
+            while k > j:
+                if nums[i] + nums[j] + nums[k] == 0:
+                    answers.append([nums[i], nums[j], nums[k]])
+                    j += 1
+                    while nums[j] == nums[j - 1] and j < k:
+                        j += 1
+                elif nums[i] + nums[j] + nums[k] > 0:
+                    k -= 1
+                else:
+                    j += 1
 
-print(answers)
+        return answers
