9cae970cbf8542b9672e9be3ad64c070ec47f4e6
tomek_jablko <trunio31@gmail.com>
2025-05-23 13:13:36 +0900

    O(n) solution

--- interview_150/array_string/two_sum_2.py
+++ interview_150/array_string/two_sum_2.py
@@ -1,8 +1,11 @@
-numbers = [0, 0, 3, 3]
-target = 0
-for i in range(len(numbers)):
-    for j in range(i + 1, len(numbers)):
-        if numbers[i] + numbers[j] == target:
-            print([i + 1, j + 1])
-        elif numbers[i] + numbers[j] > target:
-            break
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        left = 0
+        right = len(numbers) - 1
+        while numbers[left] + numbers[right] != target:
+            if numbers[left] + numbers[right] > target:
+                right -= 1
+            else:
+                left += 1
+        return [left + 1, right + 1]
+
